c8aab844e5b636e19cda79eb34c9b2ad79df664e
Magdalena Asmus <s22622@pjwstk.edu.pl>
2023-06-21 22:02:44 +0200

    Document formated and unneeded comments deleted

--- myBlog/happireshipi_app.py
+++ myBlog/happireshipi_app.py
@@ -39,8 +39,6 @@ class UserPass:
         
 
     def hash_password(self):
-        """Hash a password for storing."""
-        # the value generated using os.urandom(60)
         os_urandom_static = b"ID_\x12p:\x8d\xe7&\xcb\xf0=H1\xc1\x16\xac\xe5BX\xd7\xd6j\xe3i\x11\xbe\xaa\x05\xccc\xc2\xe8K\xcf\xf1\xac\x9bFy(\xfbn.`\xe9\xcd\xdd'\xdf`~vm\xae\xf2\x93WD\x04"
         salt = hashlib.sha256(os_urandom_static).hexdigest().encode('ascii')
         pwdhash = hashlib.pbkdf2_hmac('sha512', self.password.encode('utf-8'), salt, 100000)
@@ -48,7 +46,6 @@ class UserPass:
         return (salt + pwdhash).decode('ascii')
     
     def verify_password(self, stored_password, provided_password):
-        """Verify a stored password against one provided by user"""
         salt = stored_password[:64]
         stored_password = stored_password[64:]
         pwdhash = hashlib.pbkdf2_hmac('sha512', provided_password.encode('utf-8'), salt.encode('ascii'),  100000)
@@ -56,7 +53,7 @@ class UserPass:
         return pwdhash == stored_password
     def get_random_user_pasword(self):
 
-        password_characters = string.ascii_letters #+ string.digits + string.punctuation
+        password_characters = string.ascii_letters 
         random_password = ''.join(random.choice(password_characters)for i in range(3))
         self.password = random_password
 
